6af5294df14a8208ca040a15b358e4a972a583f6
Amol Shinde <shindeamol409@gmail.com>
2024-02-25 22:48:08 +0530

    Added new file.

---
+++ Fragments/learning-fragment/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  //let foodItems = [];
+  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
+
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      {foodItems.length === 0 && <h3>I am still hungry.</h3>}
+      <ul className="list-group">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
