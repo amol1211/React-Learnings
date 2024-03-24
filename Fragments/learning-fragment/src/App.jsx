94edd8a686ca741518cc2dbf91fa64133988746d
Amol Shinde <shindeamol409@gmail.com>
2024-03-24 13:17:06 +0530

    Added new files.

--- Fragments/learning-fragment/src/App.jsx
+++ Fragments/learning-fragment/src/App.jsx
@@ -2,6 +2,8 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
 function App() {
   //let foodItems = [];
@@ -16,9 +18,19 @@ function App() {
 
   return (
     <>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodItems items={foodItems}></FoodItems>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput></FoodInput>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+
+      {/* <Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being.
+        </p>
+      </Container> */}
     </>
   );
 }
--- Projects/1-todo-app-version-two/src/components/TodoItems.jsx
+++ Projects/1-todo-app-version-two/src/components/TodoItems.jsx
@@ -2,7 +2,7 @@ import TodoItem from "./TodoItem";
 import styles from "./TodoItems.module.css";
 const TodoItems = ({ todoItems }) => {
   return (
-    <div className={styles.itemsContainer}>
+    <div className={styles.itemsContainercd}>
       {todoItems.map((item) => (
         <TodoItem todoDate={item.dueDate} todoName={item.name}></TodoItem>
       ))}
