52b6ca4ffd9b760af08c65156c3d94f821629d0a
Amol Shinde <shindeamol409@gmail.com>
2024-04-06 16:34:40 +0530

    Added new files

--- Fragments/learning-fragment/src/App.jsx
+++ Fragments/learning-fragment/src/App.jsx
@@ -16,12 +16,21 @@ function App() {
     "Ghee",
   ];
 
+  let textToShow = "Food Item Entered by user";
+
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+    textToShow = event.target.value;
+  };
+
   return (
     <>
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
+
         <ErrorMessage items={foodItems}></ErrorMessage>
-        <FoodInput></FoodInput>
+        <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <p>{textToShow}</p>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
 
